a9205427a653f412bc801d46617d6cdd7396ecf8
Joshua Jolley <jolleyboy@gmail.com>
2015-01-15 10:42:43 -0700

    Gulp Commit

--- php/demo.php
+++ php/demo.php
@@ -1,10 +1,3 @@
 <?php 
-  echo "PHP says: Hello World!";
-  for ($i = 0; $i < 3; $i++) 
-    echo "<div>This is div $i</div>";
-
-  echo "Send email to:";
-  $email = "jolleyboy@gmail.com";
-  echo "<a href='mailto:$email'>$email</a>";
-
+  echo "PHP says hello."
 ?>
