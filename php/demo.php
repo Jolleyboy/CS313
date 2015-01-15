f20709bb6129b7e77844d2a63e70a9eb658bcdbf
Joshua Jolley <jolleyboy@gmail.com>
2015-01-15 10:41:29 -0700

    Gulp Commit

--- php/demo.php
+++ php/demo.php
@@ -3,7 +3,7 @@
   for ($i = 0; $i < 3; $i++) 
     echo "<div>This is div $i</div>";
 
-  echo "Send email to: <input id='email>\n";
+  echo "Send email to:";
   $email = "jolleyboy@gmail.com";
   echo "<a href='mailto:$email'>$email</a>";
 
